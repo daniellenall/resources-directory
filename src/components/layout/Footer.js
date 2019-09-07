4ea65d7c516b737ad007f7aeace4db57e55281d4
Danielle <daniellernall@gmail.com>
2019-09-07 23:15:53 +0100

    Added link to footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 function Footer(){
   return (
     <div class="footer">
-      <p>Contribute to this list on GitHub</p>
+      <p>Contribute to this list on <a href="https://github.com/daniellenall/resources-directory">GitHub</a></p>
     </div>
     );
 }
