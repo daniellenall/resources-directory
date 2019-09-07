cb49c9b7c98b26fa61a7fa9060c4d8aee5e77aa3
Danielle <daniellernall@gmail.com>
2019-09-07 23:16:16 +0100

    Making list of sites

--- src/sites.js
+++ src/sites.js
@@ -1,49 +1,20 @@
-export var sites = [{
-        name: 'Sample Site',
-        url: 'https://www.thinknimble.com/case-studies/imby/',
-        desc: 'A simple description',
-        categories: ['util', '', 'search']
-      },
+export var sites = [
       {
         name: 'Color Picker',
         url: 'https://www.colorhexa.com/e01051',
         desc: 'Site with color schemes and other fun stuff',
-        categories: ['util', 'design']
+        categories: ['color', 'design']
       },
       {
-        name: 'Shards Pro',
-        url: 'https://designrevision.com/demo/shards-pro/',
-        desc: 'React template kit',
-        categories: ['util', 'templates']
-      },
+        name: 'HTML Colors',
+        url: 'https://htmlcolors.com/',
+        desc: 'HTML color schemes',
+        categories: ['color']
+      }, 
       {
-        name: 'Visual Hierarchy',
-        url: 'https://visualhierarchy.co/blog/category/web-design/page/2/',
-        desc: 'Web design tips and tricks',
-        categories: ['util', 'design']
-      },
-      {
-        name: 'Web Resources',
-        url: 'https://teodor.co/web-resources/',
-        desc: 'By Teodor Nikolaev',
-        categories: ['util', 'design']
-      },
-      {
-        name: "Tania Rascia's Blog",
-        url: 'https://www.taniarascia.com/blog/',
-        desc: 'Useful links and resources',
-        categories: ['util', 'design']
-      },
-      {
-        name: 'BALupton',
-        url: 'https://balupton.com',
-        desc: 'Site with color schemes and other fun stuff',
-        categories: ['util', 'design']
-      },
-      {
-        name: 'List of Things Design',
-        url: 'http://gabrieltomescu.com/blog/my-list-of-things-design',
-        desc: 'Listing of design resources',
-        categories: ['']
+        name: 'Land-book',
+        url: 'https://land-book.com/',
+        desc: 'A collection of site landing pages',
+        categories: ['design', 'inspiration']
       }
 ];
