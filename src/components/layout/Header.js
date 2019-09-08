0557f52aec3cf0e3ecc919ec792a3d89d169d588
Danielle <daniellernall@gmail.com>
2019-09-08 21:57:03 +0100

    Changed page title

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 function Header(){
   return (
     <div class="header">
-      <h1>Title</h1>
+      <h1>Dev Resources</h1>
       <p>A collection of design resources for web developers</p>
     </div>
     );
