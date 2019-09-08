876186de0f56b1ffb1b38be38f5689978e4cc48c
Danielle <daniellernall@gmail.com>
2019-09-08 21:54:52 +0100

    Added more sites to directory

--- src/sites.js
+++ src/sites.js
@@ -16,5 +16,23 @@ export var sites = [
         url: 'https://land-book.com/',
         desc: 'A collection of site landing pages',
         categories: ['design', 'inspiration']
+      },
+      {
+        name: 'getwaves.io',
+        url: 'https://getwaves.io/',
+        desc: 'Generate SVG wave shapes',
+        categories: ['design', 'SVG', 'generator']
+      },
+      {
+        name: 'Blobmaker',
+        url: 'https://www.blobmaker.app/',
+        desc: 'Generate SVG blob shapes of varying complexity',
+        categories: ['design', 'SVG', 'generator']
+      },
+      {
+        name: 'Unsplash',
+        url: 'https://unsplash.com/',
+        desc: 'Free high quality stock images',
+        categories: ['design', 'photo', 'stock']
       }
 ];
